d86ee7e22862f79f0937ebf88de9908e2863b1a3
Ana Amaro <amarovazquezana@gmail.com>
2019-09-11 17:21:01 +0200

    add docs

---
+++ docs/precache-manifest.f3fcd75263f8c9271c52e01a667f0aa3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc393186e98d9617f69895ef62e94f68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9640043af3183eee8461",
+    "url": "./static/js/2.d8a70722.chunk.js"
+  },
+  {
+    "revision": "b25657eca0c25c0fccbe",
+    "url": "./static/js/main.fa5b1da0.chunk.js"
+  },
+  {
+    "revision": "1a2ec83433844d1337cf",
+    "url": "./static/js/runtime~main.a9a80781.js"
+  }
+]);
