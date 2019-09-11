c4c0169f3d711654b5fded3bf426e335b1453aea
Ana Amaro <amarovazquezana@gmail.com>
2019-09-11 17:14:41 +0200

    Pokemon Lists

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React from 'react';
+
+const data = [
+  {
+    id: 1,
+    name: 'bulbasaur',
+    types: ['poison', 'grass'],
+    evolution: null,
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+  },
+  {
+    id: 2,
+    name: 'ivysaur',
+    types: ['poison', 'grass'],
+    evolution: 'bulbasaur',
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'
+  },
+  {
+    id: 3,
+    name: 'venusaur',
+    types: ['poison', 'grass'],
+    evolution: 'ivysaur',
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
+  },
+  {
+    id: 4,
+    name: 'charmander',
+    types: ['fire'],
+    evolution: null,
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
+  },
+  {
+    id: 5,
+    name: 'charmeleon',
+    types: ['fire'],
+    evolution: 'charmander',
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'
+  },
+  {
+    id: 6,
+    name: 'charizard',
+    types: ['flying', 'fire'],
+    evolution: 'charmeleon',
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'
+  },
+  {
+    id: 7,
+    name: 'squirtle',
+    types: ['water'],
+    evolution: null,
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'
+  },
+  {
+    id: 8,
+    name: 'wartortle',
+    types: ['water'],
+    evolution: 'squirtle',
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png'
+  },
+  {
+    id: 9,
+    name: 'blastoise',
+    types: ['water'],
+    evolution: 'wartortle',
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png'
+  },
+  {
+    id: 10,
+    name: 'caterpie',
+    types: ['bug'],
+    evolution: null,
+    url:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png'
+  }
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1 className='title'>Mi lista de Pokemon</h1>
+        <ul className='name'>
+          {data.map((item, index) => {
+            return (
+              <li className='name' key={`p${index}`} id={`p${index}`}>
+                <div className='pokemoncard'>
+                  <h2 className='pokemon__name'>{item.name}</h2>
+                  <ul className='url'>
+                    <img src={item.url} alt='url_image' />
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
