56d1717556fb873ce030fc151e2ef888a0ff6c77
Ana Amaro <amarovazquezana@gmail.com>
2019-09-15 22:23:54 +0200

    Export pokemon JSON to Pokemon.js. Incorporate new styles to App.css

--- src/App.js
+++ src/App.js
@@ -1,101 +1,46 @@
 import React from 'react';
-//import '../stylesheets/App.css';
-
-const data = [
-  {
-    id: 1,
-    name: 'bulbasaur',
-    types: ['poison', 'grass'],
-    evolution: null,
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-  },
-  {
-    id: 2,
-    name: 'ivysaur',
-    types: ['poison', 'grass'],
-    evolution: 'bulbasaur',
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'
-  },
-  {
-    id: 3,
-    name: 'venusaur',
-    types: ['poison', 'grass'],
-    evolution: 'ivysaur',
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
-  },
-  {
-    id: 4,
-    name: 'charmander',
-    types: ['fire'],
-    evolution: null,
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
-  },
-  {
-    id: 5,
-    name: 'charmeleon',
-    types: ['fire'],
-    evolution: 'charmander',
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'
-  },
-  {
-    id: 6,
-    name: 'charizard',
-    types: ['flying', 'fire'],
-    evolution: 'charmeleon',
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'
-  },
-  {
-    id: 7,
-    name: 'squirtle',
-    types: ['water'],
-    evolution: null,
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'
-  },
-  {
-    id: 8,
-    name: 'wartortle',
-    types: ['water'],
-    evolution: 'squirtle',
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png'
-  },
-  {
-    id: 9,
-    name: 'blastoise',
-    types: ['water'],
-    evolution: 'wartortle',
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png'
-  },
-  {
-    id: 10,
-    name: 'caterpie',
-    types: ['bug'],
-    evolution: null,
-    url:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png'
-  }
-];
+import './stylesheets/App.css';
+import pokemons from './components/Pokemon';
+/*import PokeList from './components/PokeList';*/
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemons: pokemons
+    };
+  }
+
+  componentDidMount() {
+    this.getPokemons();
+  }
+
+  getPokemons() {
+    const ENDPOINT = pokemons;
+
+    fetch(ENDPOINT)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
-      <div className='app'>
-        <h1 className='title'>Mi lista de Pokemon</h1>
-        <ul className='name'>
-          {data.map((item, index) => {
+      <div className='App'>
+        <h1 className='title'>Mi lista de pokemon</h1>
+        <ul className='pokemon__list'>
+          {pokemons.map((item, index) => {
             return (
-              <li className='name' key={`p${index}`} id={`p${index}`}>
-                <img src={item.url} alt='url_image' />
-                <div className='pokemoncard'>
+              <li className='pokemon__id' key={`p${index}`} id={`p${index}`}>
+                <img src={item.url} alt='pokemon__image' />
+                <div className='pokemon__card'>
                   <h2 className='pokemon__name'>{item.name}</h2>
+                  <p className='pokemon__types'>{item.types}</p>
                 </div>
               </li>
             );
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,1 +1,26 @@
-//JSON Pokemon List
+/*import React from 'react';
+
+class PokeList extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1 className='title'>Mi lista de pokemon</h1>
+        <ul className='name'>
+          {pokemons.map((item, index) => {
+            return (
+              <li className='name' key={`p${index}`} id={`p${index}`}>
+                <img src={item.url} alt='url_image' />
+                <div className='pokemon__card'>
+                  <h2 className='pokemon__name'>{item.name}</h2>
+                  <p className='pokemon__types'>{item.types}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default PokeList;*/
